ba70ca30de09834577b21889496c81459ccf91b0
ThomasRC-bit <cunningham.thomas.20@c-dh.org>
2020-02-12 18:02:05 -0600

    AddingEndToConveyorCommands

--- WindChill_2020/src/main/java/frc/robot/commands/ConveyorBackwards.java
+++ WindChill_2020/src/main/java/frc/robot/commands/ConveyorBackwards.java
@@ -42,6 +42,7 @@ public class ConveyorBackwards extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_subsystem.conveyorMotor.set(0);
   }
 
   // Returns true when the command should end.
--- WindChill_2020/src/main/java/frc/robot/commands/ConveyorForward.java
+++ WindChill_2020/src/main/java/frc/robot/commands/ConveyorForward.java
@@ -41,7 +41,8 @@ public class ConveyorForward extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
+  public void end(boolean interrupted) {    
+    m_subsystem.conveyorMotor.set(0);
   }
 
   // Returns true when the command should end.
